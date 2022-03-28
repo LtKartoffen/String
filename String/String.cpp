489b5b33fbd7d963d8564cdff2c00c885410a289
Юрій Краснюк <youra663@kpi.ua>
2022-03-28 16:07:00 +0300

    modified String class with each other constructors etc. Added sayings.cpp

--- String/String.cpp
+++ String/String.cpp
@@ -1,9 +1,17 @@
 #include "String.h"
 #include <cstring>
+//define _CRT_SECURE_NO_WARNINGS
 using std::cout;
+using std::cin;
 
 // Initialize static-member of class
 int String::num_strings = 0;
+
+// Static method
+int String::HowMany()
+{
+	return num_strings;
+}
 
 // String's methods
 
@@ -14,30 +22,98 @@ String::String(const char* s)
 	str = new char[len + 1];
 	std::strcpy(str, s);
 	num_strings++;
-	cout << num_strings << ": \"" << str
-		<< "\" object created\n";
+	//cout << num_strings << ": \"" << str
+	//	<< "\" object created\n";
 }
 
 String::String()
 {
-	len = 4;
-	str = new char[4];
-	std::strcpy(str, "C++");
+	len = 1;
+	str = new char[1];
+	str[0] = '\0';
 	num_strings++;
-	cout << num_strings << ": \"" << str
-		<< "\" default object created\n";
+	//cout << num_strings << ": \"" << str
+	//	<< "\" default object created\n";
+}
+
+String::String(const String& st)
+{
+	num_strings++;
+	len = st.len;
+	str = new char[len + 1];
+	std::strcpy(str, st.str);
 }
 
 String::~String()
 {
-	cout << "\"" << str << "\" object deleted, ";
+	//cout << "\"" << str << "\" object deleted, ";
 	num_strings--;
-	cout << num_strings << " left\n";
+	//cout << num_strings << " left\n";
 	delete[] str;
 }
 
+// Assign operators
+// Assing String to String
+String& String::operator=(const String& st)
+{
+	if (this == &st)
+		return *this;
+	delete[] str;
+	len = st.len;
+	str = new char[len + 1];
+	std::strcpy(str, st.str);
+	return *this;
+}
+
+// Assign String to char* 
+String& String::operator=(const char* s)
+{
+	delete[] str;
+	len = std::strlen(s);
+	str = new char[len + 1];
+	std::strcpy(str, s);
+	return *this;
+}
+
+// read and input access for some symbols in non-constant String
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+// read acces for some symbols in constant String
+const char& String::operator[](int i) const
+{
+	return str[i];
+}
+
+// Friend funcs for refreshed operations
+bool operator<(const String& st1, const String& st2)
+{
+	return (std::strcmp(st1.str, st2.str) < 0);
+}
+bool operator>(const String& st1, const String& st2)
+{
+	return st2.str < st1.str;
+}
+bool operator==(const String& st1, const String& st2)
+{
+	return (std::strcmp(st1.str, st2.str) == 0);
+}
+// Simple output String
 std::ostream& operator<<(std::ostream& os, const String& st)
 {
 	os << st.str;
 	return os;
+}
+// Simple input String
+std::istream& operator>>(std::istream& is, String& st)
+{
+	char temp[String::CINLIM];
+	is.get(temp, String::CINLIM);
+	if (is)
+		st = temp;
+	while (is and is.get() != '\n')
+		continue;
+	return is;
 }
--- String/String.h
+++ String/String.h
@@ -6,14 +6,30 @@ class String
 private:
 	char* str;
 	int len;
-	static int num_strings;
+	static int num_strings;						//num of objects
+	static const int CINLIM = 80;				//limit for console input
 public:
-	String(const char* s);
-	String();
-	~String();
-	
+	String(const char* s);						//constructor by default
+	String();									//constructor
+	~String();									//destructor
+	String(const String&);						//constructor of copy
+	int lenght() const { return len; }		
+
+	//Methods of refreshed operations
+	String& operator=(const String&);
+	String& operator=(const char*);
+	char& operator[](int i);
+	const char& operator[](int i) const;
+
 	//friend-func
+	friend bool operator<(const String& st, const String& st2);
+	friend bool operator>(const String& st, const String& st2);
+	friend bool operator==(const String& st, const String& st2);
+	friend std::istream& operator>>(std::istream& is, String& st);
 	friend std::ostream& operator<<(std::ostream& os, const String& st);
+
+	//Static-func
+	static int HowMany();
 };
 
 #endif // !STRING_H_
